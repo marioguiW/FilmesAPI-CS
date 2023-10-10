1a47085b6586e8235084688d319a7350c962e257
MarioGuiW <marioguilhermew@gmail.com>
2023-10-10 17:55:46 -0300

    1/3

---
+++ FimesAPI/Controllers/FilmeController.cs
@@ -0,0 +1,36 @@
+﻿using FimesAPI.Modules;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FimesAPI.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+
+public class FilmeController : ControllerBase
+{
+    private static List<Filme> filmes = new();
+
+    private static int id = 0;
+
+    [HttpPost]
+    public void AdicionaFilme([FromBody] Filme filme)
+    {
+        filme.Id = id++;
+        filmes.Add(filme);
+        Console.WriteLine(filme.Titulo);
+        Console.WriteLine(filme.Duracao);
+    }
+
+    [HttpGet]
+    public List<Filme> ExibeFilme()
+    {
+        return filmes;
+    }
+
+    [HttpGet("{id}")]
+
+    public Filme? BuscaFilme(int id)
+    {
+        return filmes.FirstOrDefault(filme => filme.Id == id);
+    }
+}
---
+++ FimesAPI/Modules/Filme.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FimesAPI.Modules;
+
+public class Filme
+{
+    public int Id { get; set; }
+    [Required(ErrorMessage = "O titulo do filme deve ser obrigatorio")]
+    public string Titulo {  get; set; }
+
+    [Required(ErrorMessage = "O genero do filme deve ser obrigatorio")]
+    [MaxLength(50, ErrorMessage = "O genero não pode ultrapassar 50 caracteres")]
+    public string Genero { get; set; }
+    [Required(ErrorMessage = "A duração do filme deve ser obrigatoria")]
+    [Range(70,600, ErrorMessage = "A duração deve ser entre 70 e 600 minutos")]
+    public int Duracao { get; set; }
+}
